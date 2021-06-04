9ba14c3229aff6e0989d3378a7ef485d1a927819
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-06-04 22:13:50 +0200

    finished node/express/ejs

--- ex_pbLanguage_basicSkillsExpress.js
+++ ex_pbLanguage_basicSkillsExpress.js
@@ -4,9 +4,14 @@ const express = require('express');
 const app = express();
 const port = 3010;
 app.set('view engine', 'ejs');
+app.set('views','./express_views');
 
 app.get('/', (req, res) => {
-	res.render('index');
+	res.render('index',
+		{
+			answer001: findCertainCities('Bordeaux')
+		}
+	);
 });
 
 app.listen(port, () => {
